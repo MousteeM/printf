4288fcd619335bc2e930e8ebd6540cac7bdca9ed
MousteeM <Moustee01@gmail>
2022-10-15 08:59:32 +0000

    initializing _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - print output according to specified format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will ....
+ * Return: printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
