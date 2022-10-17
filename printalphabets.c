723aed40fb3fb6ae0fbd4496c293f8d3f61d5281
MousteeM <moustee01@gmail.com>
2022-10-17 12:20:41 +0000

    created function to print strings and characters

---
+++ printalphabets.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * printChar - prints a character
+ * @arguments: va_list arguments from _printf
+ * 
+ * Return: number of char printed
+ */
+int printChar(va_list arguments)
+{
+	_putchar(va_arg(arguments, int));
+	return (1);
+}
+
+/**
+ * printString - loops through a string and prints
+ * every character
+ * @arguments: va_list arguments from _printf
+ * Return: number of char printed
+ */
+int printString(va_list arguments)
+{
+	char *s = va_arg(arguments, char *);
+
+	if (!s)
+		s = "(null)";
+	return (_puts(s));
+}
