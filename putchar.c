5d2f4f714ea7725f15322b55cafa1915104ac843
MousteeM <moustee01@gmail.com>
2022-10-18 15:44:15 +0000

    Added _puts function

--- putchar.c
+++ putchar.c
@@ -13,3 +13,17 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
